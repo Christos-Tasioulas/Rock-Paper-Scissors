1d7fca6cd14d5603e77d2554db3bc5b1ff25035c
rtasioul <rtasioul@netcompany.com>
2024-12-03 10:35:59 +0200

    Cleanup BasicRuleSet

--- src/main/java/gameAndStats/rulesets/BasicRuleSet.java
+++ src/main/java/gameAndStats/rulesets/BasicRuleSet.java
@@ -1,10 +1,9 @@
 package gameAndStats.rulesets;
 
 import gameAndStats.Difficulties;
-import gameAndStats.entities.*;
-
-import java.util.List;
-import java.util.Optional;
+import gameAndStats.entities.Paper;
+import gameAndStats.entities.Rock;
+import gameAndStats.entities.Scissors;
 
 public class BasicRuleSet extends RuleSet {
 
@@ -14,38 +13,5 @@ public class BasicRuleSet extends RuleSet {
         allEntities.add(new Paper());
         allEntities.add(new Scissors());
         createRules();
-    }
-
-    private void createRulesForEntity(Entity e) {
-        addEntitiesItLosesFrom(e);
-        addEntitiesItBeats(e);
-    }
-
-    protected void createRules() {
-        for (Entity entity : allEntities) {
-            createRulesForEntity(entity);
-        }
-    }
-
-    private void addEntitiesItBeats(Entity e) {
-        List<String> weakerEntities = e.getWeakerEntities();
-        for (String entity : weakerEntities) {
-            e.addEntityItBeats(findEntityByType(entity));
-        }
-    }
-
-    private void addEntitiesItLosesFrom(Entity e) {
-        List<String> strongerEntities = e.getStrongerEntities();
-        for (String entity : strongerEntities) {
-            e.addEntityItLosesFrom(findEntityByType(entity));
-        }
-    }
-
-    public static Entity findEntityByType(String type) {
-        Optional<Entity> entity = allEntities.stream()
-                .filter(p -> p.getEntityType().equals(type))
-                .filter(p -> p.isInValidDifficulties(Difficulties.BASIC))
-                .findFirst();
-        return entity.orElse(null);
     }
 }
